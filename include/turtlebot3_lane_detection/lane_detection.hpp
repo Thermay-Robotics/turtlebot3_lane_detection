077c397b4e4cff903a8fec4be193f561a61bd7c3
totordudu <toto.duvinage@gmail.com>
2022-06-08 14:09:33 +0200

    refreshing code

--- include/turtlebot3_lane_detection/lane_detection.hpp
+++ include/turtlebot3_lane_detection/lane_detection.hpp
@@ -10,10 +10,15 @@
 #include <algorithm>
 #include <numeric>
 #include <cstdlib>
+#include <string>
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
+#include <sensor_msgs/image_encodings.h>
 
 #include "turtlebot3_lane_detection/line_msg.h"
 
@@ -21,30 +26,32 @@ class LaneDetection
 {
 private:
     ros::NodeHandle nh;
-    ros::Publisher qr_pub;
+    ros::Publisher lane_pub;
+
+    image_transport::Publisher image_lane_pub;
     image_transport::Subscriber camera_sub;
     image_transport::ImageTransport it;
 
     std::vector<cv::Vec2d> mid_lane;
 
-    void imageCallback(const sensor_msgs::ImageConstPtr& msg); 
+    void imageCallback(const sensor_msgs::ImageConstPtr &msg);
 
     cv::Mat CannyThreshold(cv::Mat &im);
     cv::Mat defineROI(cv::Mat &im);
 
     void detectLines(cv::Mat &im, std::vector<cv::Vec4i> &lines);
-    void displayLines(cv::Mat &im,std::string name, std::vector<cv::Vec4i> &lines);
+    void displayLines(cv::Mat &im, std::string name, std::vector<cv::Vec4i> &lines);
 
-    std::vector<cv::Vec4i> computeAverageLines(cv::Mat &im,std::vector<cv::Vec4i> &lines);
+    std::vector<cv::Vec4i> computeAverageLines(cv::Mat &im, std::vector<cv::Vec4i> &lines);
     cv::Vec4i makePoint(cv::Mat &im, cv::Vec2d &r);
-    void polyfit(cv::Vec4i &p,cv::Vec2d &param);
+    void polyfit(cv::Vec4i &p, cv::Vec2d &param);
     cv::Vec2d computeMeanLane(std::vector<cv::Vec2d> &);
 
+    cv::Mat computeMeamLaneBoundingBox(cv::Mat &im);
 
 public:
-    LaneDetection(/* args */);
-    ~LaneDetection();
+    LaneDetection();
+    ~LaneDetection(){};
 };
-
 
 #endif
--- src/lane_detection.cpp
+++ src/lane_detection.cpp
@@ -1,162 +1,82 @@
 #include "turtlebot3_lane_detection/lane_detection.hpp"
 
-
-LaneDetection::LaneDetection(): it(nh)
+LaneDetection::LaneDetection() : it(nh)
 {
-    cv::namedWindow("view");
-    cv::namedWindow("imgCanny");
-    cv::namedWindow("imgROI",cv::WINDOW_AUTOSIZE);
-    cv::namedWindow("imgLines");
-    cv::namedWindow("LaneDetection");
-    cv::namedWindow("contour");
-    //qr_pub = nh.advertise<LaneDetection::line_msg>("/line_detection", 1000);
+    lane_pub = nh.advertise<turtlebot3_lane_detection::line_msg>("/lane_detection/angle", 1000);
     camera_sub = it.subscribe("/camera/color/image_raw", 1, &LaneDetection::imageCallback, this);
+
+    // Image publishers
+    image_lane_pub = it.advertise("/lane_detection/image/image_lane", 1);
 }
 
-LaneDetection::~LaneDetection()
+void LaneDetection::imageCallback(const sensor_msgs::ImageConstPtr &msg)
 {
-    cv::destroyAllWindows(); 
-}
-
-void LaneDetection::imageCallback(const sensor_msgs::ImageConstPtr& msg){
     cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
 
     cv::Mat image_canny = CannyThreshold(image);
 
     cv::Mat imageRoi = defineROI(image_canny);
-    cv::Mat line_img;
     cv::Mat img_LaneDetection = defineROI(image);
 
     std::vector<cv::Vec4i> lines; // will hold the results of the detection
     detectLines(imageRoi, lines);
-    cv::cvtColor(imageRoi, line_img, cv::COLOR_GRAY2BGR );
-    displayLines(line_img,"imgLines", lines);
 
-    std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(imageRoi,contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    cv::Mat image_lane = computeMeamLaneBoundingBox(imageRoi);
 
-    std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
-    std::vector<cv::RotatedRect> boundRect( contours.size() );
-
-    for( size_t i = 0; i < contours.size(); i++ )
-    {
-        //approxPolyDP( contours[i], contours_poly[i], 3, true );
-        //boundRect[i] = boundingRect( contours_poly[i] );
-        boundRect[i] = minAreaRect( contours[i] );
-    }    
-
-    //Draw the contours
-    cv::Mat contourImage(imageRoi.size(), CV_8UC3, cv::Scalar(0,0,0));
-    cv::Scalar colors[3];
-    colors[0] = cv::Scalar(255, 0, 0);
-    colors[1] = cv::Scalar(0, 255, 0);
-    colors[2] = cv::Scalar(0, 0, 255);
-
-    cv::Vec4i meanLaneCoordinates(0,0,0,0);
-    for (size_t idx = 0; idx < contours.size(); idx++) {
-        cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
-        //cv::rectangle(contourImage, boundRect[idx].tl(), boundRect[idx].br(), colors[idx % 3], 2 );
-    
-        cv::Point2f rect_points[4];
-        boundRect[idx].points( rect_points );
-
-        for ( int j = 0; j < 4; j++ )
-        {
-            std::cout << rect_points[j] << " ";
-            bool error_correction = false;
-            if(j == 0){
-                // To manage opencv vector error 
-                if(rect_points[j].x - rect_points[j+1].x < rect_points[j+3].x - rect_points[j].x){
-                    meanLaneCoordinates[j] +=  (rect_points[j].x + rect_points[j+1].x)/2;
-                    meanLaneCoordinates[j+1] +=  (rect_points[j].y + rect_points[j+1].y)/2;
-                    error_correction = true;
-                }
-                else{
-                    meanLaneCoordinates[j] +=  (rect_points[j+3].x + rect_points[j].x)/2;
-                    meanLaneCoordinates[j+1] +=  (rect_points[j+3].y + rect_points[j].y)/2;
-                }
-            }
-            
-            if(j==2){
-                if((rect_points[j].x - rect_points[j+1].x < rect_points[j+3].x - rect_points[j].x)){
-                    meanLaneCoordinates[j] +=  (rect_points[j].x + rect_points[j+1].x)/2;
-                    meanLaneCoordinates[j+1] +=  (rect_points[j].y + rect_points[j+1].y)/2;
-                }
-                else{
-                    meanLaneCoordinates[j] +=  (rect_points[j-1].x + rect_points[j].x)/2;
-                    meanLaneCoordinates[j+1] +=  (rect_points[j-1].y + rect_points[j].y)/2;
-                }
-            }
-
-            line(contourImage, rect_points[j], rect_points[(j+1)%4], colors[idx%3]);
-        }
-        std::cout << std::endl;
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        meanLaneCoordinates[i] /= contours.size();
-    }
-
-    line( contourImage, cv::Point(meanLaneCoordinates[0],meanLaneCoordinates[1]),cv::Point(meanLaneCoordinates[2],meanLaneCoordinates[3]),cv::Scalar(0,0,255) );
-    
-
-
-    cv::imshow("contour", contourImage);
-
-    std::vector<cv::Vec4i> lines_avg = computeAverageLines(imageRoi,lines);
-    displayLines(img_LaneDetection,"LaneDetection",lines_avg);
-
-
-    cv::imshow("imgCanny", image_canny);
-    cv::imshow("imgROI", imageRoi);
-    cv::imshow("view",image);
+    cv_bridge::CvImagePtr cv_ptr(new cv_bridge::CvImage);
+    cv_ptr->encoding = "bgr8";
+    cv_ptr->header.stamp = ros::Time::now();
+    cv_ptr->header.frame_id = "camera_color_frame";
+    cv_ptr->image = image_lane;
 
     cv::waitKey(30);
+
+    image_lane_pub.publish(cv_ptr->toImageMsg());
 }
 
 /**
  * @brief Canny Threshold camera image
- * 
+ *
  * // TODO find a better way to add parameters
- * 
- * @param im 
- * @return cv::Mat 
+ *
+ * @param im
+ * @return cv::Mat
  */
-cv::Mat LaneDetection::CannyThreshold(cv::Mat &im){
+cv::Mat LaneDetection::CannyThreshold(cv::Mat &im)
+{
     cv::Mat imgGrayScale;
     cv::Mat imgBlurred;
     cv::Mat imgCanny;
-    cv::cvtColor(im, imgGrayScale, CV_BGR2GRAY);        // convert to grayscale
+    cv::cvtColor(im, imgGrayScale, CV_BGR2GRAY); // convert to grayscale
 
+    cv::GaussianBlur(imgGrayScale,   // input image
+                     imgBlurred,     // output image
+                     cv::Size(5, 5), // smoothing window width and height in pixels
+                     1.5);           // sigma value, determines how much the image will be blurred
 
-    cv::GaussianBlur(imgGrayScale,  // input image
-        imgBlurred,                 // output image
-        cv::Size(5, 5),             // smoothing window width and height in pixels
-        1.5);                       // sigma value, determines how much the image will be blurred
-
-    cv::Canny(imgBlurred,           // input image
-        imgCanny,                   // output image
-        50,                        // low threshold
-        150);                       // high threshold    
+    cv::Canny(imgBlurred, // input image
+              imgCanny,   // output image
+              50,         // low threshold
+              150);       // high threshold
 
     return imgCanny;
 }
 
 /**
  * @brief Define region of Interest
- * 
+ *
  * // TODO parameters parsing
- * 
- * @param im 
- * @return cv::Mat 
+ *
+ * @param im
+ * @return cv::Mat
  */
-cv::Mat LaneDetection::defineROI(cv::Mat &im){
+cv::Mat LaneDetection::defineROI(cv::Mat &im)
+{
     cv::Mat imgROI(im);
     int heigth = im.size().height;
     int width = im.size().width;
 
-    cv::Rect Rec((width/2) - 200, heigth - 200, 400, 200);
+    cv::Rect Rec((width / 2) - 200, heigth - 200, 400, 200);
 
     cv::Mat ROI = imgROI(Rec);
 
@@ -165,47 +85,51 @@ cv::Mat LaneDetection::defineROI(cv::Mat &im){
 
 /**
  * @brief Standard Hough Line Transform
- * 
- * @param im 
- * @param lines 
+ *
+ * @param im
+ * @param lines
  */
-void LaneDetection::detectLines(cv::Mat &im, std::vector<cv::Vec4i> &lines){
-    HoughLinesP(im, lines, 1, CV_PI/180, 5, 40, 10); // runs the actual detection
+void LaneDetection::detectLines(cv::Mat &im, std::vector<cv::Vec4i> &lines)
+{
+    HoughLinesP(im, lines, 1, CV_PI / 180, 5, 40, 10); // runs the actual detection
 }
 
 /**
  * @brief Display line based on OpenCv example
- * 
- * @param im 
- * @param lines 
+ *
+ * @param im
+ * @param lines
  */
-void LaneDetection::displayLines(cv::Mat &im,std::string name,std::vector<cv::Vec4i> &lines){
-    for( size_t i = 0; i < lines.size(); i++ )
+void LaneDetection::displayLines(cv::Mat &im, std::string name, std::vector<cv::Vec4i> &lines)
+{
+    for (size_t i = 0; i < lines.size(); i++)
     {
         line(im, cv::Point(lines[i][0], lines[i][1]),
-        cv::Point( lines[i][2], lines[i][3]), cv::Scalar(0,0,255), 3, 8 );
+             cv::Point(lines[i][2], lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
     }
 
     cv::imshow(name, im);
 }
 
-void LaneDetection::polyfit(cv::Vec4i &p,cv::Vec2d &param){
-    param[0]= (double)(p[3] - p[1])/(double)(p[2]-p[0]);
+void LaneDetection::polyfit(cv::Vec4i &p, cv::Vec2d &param)
+{
+    param[0] = (double)(p[3] - p[1]) / (double)(p[2] - p[0]);
     param[1] = p[1] - param[0] * p[0];
 }
 
 /**
- * @brief Compute the average for left ang right lanes 
- * 
- * @param lines 
+ * @brief Compute the average for left ang right lanes
+ *
+ * @param lines
  */
-std::vector<cv::Vec4i> LaneDetection::computeAverageLines(cv::Mat &im,std::vector<cv::Vec4i> &lines){
+std::vector<cv::Vec4i> LaneDetection::computeAverageLines(cv::Mat &im, std::vector<cv::Vec4i> &lines)
+{
     mid_lane.clear();
 
-    for( size_t i = 0; i < lines.size(); i++ )
+    for (size_t i = 0; i < lines.size(); i++)
     {
         cv::Vec2d parameters;
-        polyfit(lines[i],parameters);
+        polyfit(lines[i], parameters);
 
         mid_lane.push_back(parameters);
     }
@@ -218,38 +142,109 @@ std::vector<cv::Vec4i> LaneDetection::computeAverageLines(cv::Mat &im,std::vecto
 
 /**
  * @brief Compute of one lane
- * 
- * @param l lane 
- * @return cv::Vec2d 
+ *
+ * @param l lane
+ * @return cv::Vec2d
  */
-cv::Vec2d LaneDetection::computeMeanLane(std::vector<cv::Vec2d> &l){
+cv::Vec2d LaneDetection::computeMeanLane(std::vector<cv::Vec2d> &l)
+{
     cv::Vec2d mean;
 
     double sum_slope = 0;
     double sum_y = 0;
 
-    for(auto param : l){
+    for (auto param : l)
+    {
         sum_slope += param[0];
         sum_y += param[1];
     }
 
     mean[0] = sum_slope / l.size();
     mean[1] = sum_y / l.size();
-    
+
     return mean;
 }
 
-cv::Vec4i LaneDetection::makePoint(cv::Mat &im, cv::Vec2d &r){
+cv::Vec4i LaneDetection::makePoint(cv::Mat &im, cv::Vec2d &r)
+{
     double slope = r[0];
     double y_int = r[1];
 
     cv::Vec4i point;
 
     point[1] = im.size().height;
-    point[3] = (int)(point[1]*(3/5));
+    point[3] = (int)(point[1] * (3 / 5));
 
     point[0] = (int)((point[1] - y_int) / slope);
     point[2] = (int)((point[3] - y_int) / slope);
 
     return point;
+}
+
+/**
+ * @brief This method is used to compute the average lane from the bounding boxes
+ *
+ * @param im
+ * @return cv::Vec2d
+ */
+cv::Mat LaneDetection::computeMeamLaneBoundingBox(cv::Mat &im)
+{
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(im, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+    std::vector<std::vector<cv::Point>> contours_poly(contours.size());
+    std::vector<cv::RotatedRect> boundRect(contours.size());
+
+    // //Draw the contours
+    cv::Mat contourImage(im.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Scalar colors[3];
+    colors[0] = cv::Scalar(255, 0, 0);
+    colors[1] = cv::Scalar(0, 255, 0);
+    colors[2] = cv::Scalar(0, 0, 255);
+
+    cv::Point2f mean_center;
+    float mean_angle = 0;
+
+    int line_lenght = 100;
+
+    for (size_t idx = 0; idx < contours.size(); idx++)
+    {
+        boundRect[idx] = cv::minAreaRect(contours[idx]);
+
+        mean_center += boundRect[idx].center;
+
+        // This condition is used to overcome strange opencv's behaviour
+        // see https://namkeenman.wordpress.com/2015/12/18/open-cv-determine-angle-of-rotatedrect-minarearect/ to understand
+        if (boundRect[idx].size.width < boundRect[idx].size.height)
+            mean_angle += abs(boundRect[idx].angle);
+        else
+            mean_angle -= 90 - abs(boundRect[idx].angle);
+
+        cv::Point2f rect_points[4];
+        boundRect[idx].points(rect_points);
+
+        for (int j = 0; j < 4; j++)
+        {
+            line(contourImage, rect_points[j], rect_points[(j + 1) % 4], colors[idx % 3]);
+        }
+    }
+
+    mean_center.x /= contours.size();
+    mean_center.y /= contours.size();
+    mean_angle /= contours.size();
+
+    // Create line msg and publish it
+    turtlebot3_lane_detection::line_msg msg;
+    msg.angle = mean_angle;
+    lane_pub.publish(msg);
+
+    // Create line for display
+    cv::Point mean_line;
+    mean_line.x = (int)round(mean_center.x - line_lenght * sin(mean_angle * CV_PI / 180.0));
+    mean_line.y = (int)round(mean_center.y - line_lenght * cos(mean_angle * CV_PI / 180.0));
+
+    // Display line
+    line(contourImage, mean_center, mean_line, cv::Scalar(0, 0, 255));
+
+    return contourImage;
 }
